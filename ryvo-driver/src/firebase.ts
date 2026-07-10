6e8918a6d9b6bed4abb718ce704ca4287a2e8ffd
23btrct005-coder <23btrct005@jainuniversity.ac.in>
2026-07-11 01:19:28 +0530

    feat: setup firebase configuration

---
+++ ryvo-driver/src/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfK5o61sdk2NUSu35uT2Lj4C50uQGBnM8",
+  authDomain: "ryvo-7addb.firebaseapp.com",
+  projectId: "ryvo-7addb",
+  storageBucket: "ryvo-7addb.firebasestorage.app",
+  messagingSenderId: "676787706675",
+  appId: "1:676787706675:web:a9c3e3dcd06f87731cbc46",
+  measurementId: "G-R3PL5TSWV8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+export const db = getFirestore(app);
+export const auth = getAuth(app);
